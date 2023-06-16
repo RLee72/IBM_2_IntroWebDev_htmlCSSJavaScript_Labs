fc04a2b576cd375cc29ea078b1e64d9055a8c98a
Rian L <32441927+RLee72@users.noreply.github.com>
2023-06-15 23:26:34 -0400

    Update script.js
    added weight function

--- unit-conversion/script.js
+++ unit-conversion/script.js
@@ -3,3 +3,9 @@
     var fahrenheit = (celsius * 9/5) + 32
     document.getElementById("f").value = fahrenheit
 }
+
+function weight() {
+  var kilograms = document.getElementById("kg").value;
+  var pounds = kilograms * 2.2
+  document.getElementById("lbs").value = pounds
+}
