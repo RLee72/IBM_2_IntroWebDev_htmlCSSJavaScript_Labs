203427f606935d612342352b320117dd01eb46f0
Rian L <32441927+RLee72@users.noreply.github.com>
2023-06-15 23:26:50 -0400

    Update script.js

--- unit-conversion/script.js
+++ unit-conversion/script.js
@@ -1,4 +1,4 @@
-  function temperature() {
+function temperature() {
     var celsius = document.getElementById("c").value;
     var fahrenheit = (celsius * 9/5) + 32
     document.getElementById("f").value = fahrenheit
